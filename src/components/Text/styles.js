4e878351c5c0db7b452db83c561518afa6d1621c
Charles Godoy <charles1218.cg@gmail.com>
2021-06-30 21:41:23 -0300

    subindo arquivos aula

--- src/components/Text/styles.js
+++ src/components/Text/styles.js
@@ -4,14 +4,17 @@ const styles = makeStyles(() => ({
 	title: {
 		fontFamily: "lato,sans-serif",
 		fontSize: 60,
+		color:"#2d3748",
 	},
 	subtitle: {
 		fontFamily: "lato,sans-serif",
 		fontSize: 24,
+		color:"#2d3748",
 	},
 	body: {
 		fontFamily: "lato,sans-serif",
 		fontSize: 14,
+		color:"#2d3748",
 	},
 }));
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SectionIntro from './Home/SectionIntro';
 import Home from './Home/index';
 
 
@@ -8,7 +7,6 @@ import Home from './Home/index';
 ReactDOM.render(
   <React.StrictMode>
     <Home/>
-    <SectionIntro/>
   </React.StrictMode>,
   document.getElementById('root')
 );
